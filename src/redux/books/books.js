8b620421a5291a14a83debebb30e058dfbe681fc
Betsegaw Sebsibe <betsesebe@gmail.com>
2022-09-22 15:01:52 +0300

    Add api to module

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,16 +1,12 @@
-import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
 
 const ADD_BOOK = 'bookstore/books/addBook';
 const REMOVE_BOOK = 'bookstore/books/removeBook';
+const GET_BOOK = 'bookstore/books/getBook';
+const baseURL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/50oM5lkEUSi6Y8skkWBf/books/';
 
 const allBooks = {
-  books: [
-    {
-      id: uuidv4(),
-      title: 'The power of Habit',
-      author: 'James Clear',
-    },
-  ],
+  books: [],
 };
 
 const addBook = (book) => ({
@@ -18,10 +14,44 @@ const addBook = (book) => ({
   payload: book,
 });
 
-const removeBook = (book) => ({
+const removeBook = (id) => ({
   type: REMOVE_BOOK,
-  book,
+  payload: id,
 });
+
+const uploadBook = (book) => (dispatch) => {
+  axios.post(baseURL, book).then(() => {
+    dispatch(addBook(book));
+  });
+};
+
+const getBook = (books) => ({
+  type: GET_BOOK,
+  payload: books,
+});
+
+const getBooksList = () => (dispatch) => {
+  axios.get(baseURL)
+    .then((result) => {
+      const res = result.data;
+      const newBooks = [];
+      Object.keys(res).forEach((key) => {
+        if (key) {
+          newBooks.push({
+            ...res[key][0],
+            item_id: key,
+          });
+        }
+      });
+      dispatch(getBook(newBooks));
+    });
+};
+
+const deleteBook = (id) => (dispatch) => {
+  axios.delete(baseURL + id).then(() => {
+    dispatch(removeBook(id));
+  });
+};
 
 const bookReducer = (state = allBooks, action) => {
   switch (action.type) {
@@ -31,7 +61,11 @@ const bookReducer = (state = allBooks, action) => {
       };
     case REMOVE_BOOK:
       return {
-        books: [...state.books.filter((i) => i.id !== action.book.id)],
+        books: [...state.books.filter((i) => i.item_id !== action.payload)],
+      };
+    case GET_BOOK:
+      return {
+        books: action.payload,
       };
     default:
       return state;
@@ -39,4 +73,6 @@ const bookReducer = (state = allBooks, action) => {
 };
 
 export default bookReducer;
-export { addBook, removeBook };
+export {
+  addBook, removeBook, uploadBook, deleteBook, getBooksList,
+};
