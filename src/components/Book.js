728be66c0b3f7dc932474ade2f2b942709210a39
Betsegaw Sebsibe <betsesebe@gmail.com>
2022-09-22 18:04:34 +0300

    Add styling

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { CircularProgressbar } from 'react-circular-progressbar';
 import { deleteBook } from '../redux/books/books';
+import 'react-circular-progressbar/dist/styles.css';
 
 function Book(props) {
   const bookData = props;
@@ -10,6 +12,9 @@ function Book(props) {
   const removeNow = (m) => {
     dispatch(deleteBook(m));
   };
+  const min = 1;
+  const max = 100;
+  const value = Math.floor(Math.random() * (max - min + 1) + min);
 
   return (
     <>
@@ -17,16 +22,38 @@ function Book(props) {
         <div key={i.item_id} className="flex justify-between border border-#94a3b8 m-10 sm:mx-20 sm:my-10 p-6 bg-white">
           <div className="flex flex-col gap-7">
             <div>
-              <h3 className="text-base font-semibold text-slate-500 ">{i.genre}</h3>
+              <h3 className="text-base font-semibold text-slate-500 ju ">{i.category}</h3>
               <h2 className="text-2xl font-bold">{i.title}</h2>
               <p className="text-lg book-author text-sky-600">{i.author}</p>
             </div>
-            <button type="button" className="text-base bg-blue-800 text-white py-2 sm:text-sky-600 sm:bg-transparent sm:w-1/12 sm:text-left" onClick={() => removeNow(i.item_id)}>Remove</button>
+            <div className="flex gap-4">
+              <button type="button" className="w-24 border-r-2 text-sky-600 bg-transparent text-left">Comment</button>
+              <button type="button" className="w-20 border-r-2 text-sky-600 bg-transparent text-left" onClick={() => removeNow(i.item_id)}>Remove</button>
+              <button type="button" className="w-24 text-sky-600 bg-transparent text-left">Edit</button>
+            </div>
           </div>
-          <div className="w-1/4 hidden sm:block">
-            <h3 className="text-base font-semibold text-slate-500 ">Current Chapter</h3>
-            <h2 className="text-2xl font-bold my-3">Chapter 17</h2>
-            <button type="button" className="bg-sky-400 text-white rounded py-2 px-16 mt-2px">Update Progress</button>
+          <div className="hidden md:block">
+            <div className=" flex items-center gap-12 mr-16">
+              <div className="hidden lg:block">
+                <div className="flex gap-6 pr-10 items-center lg:border-r-2 lg:h-20">
+                  <div className="w-20 h-32 flex items-center">
+                    <CircularProgressbar value={value} />
+                  </div>
+                  <div className="flex flex-col justify-center">
+                    <h2 className="text-3xl font-narmal">
+                      {value}
+                      %
+                    </h2>
+                    <h3 className="text-base font-semibold text-slate-500 ">Completed</h3>
+                  </div>
+                </div>
+              </div>
+              <div className="w-max">
+                <h3 className="text-base font-semibold text-slate-500 ">Current Chapter</h3>
+                <h2 className="text-2xl font-bold my-3">Chapter 17</h2>
+                <button type="button" className="bg-sky-400 text-white rounded py-2 px-16 mt-2px">Update Progress</button>
+              </div>
+            </div>
           </div>
         </div>
       ))}
